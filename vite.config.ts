6b825e27c1b68e1c77100cd7680ea54c8671b315
Mohamed Najahi <127703476+MohamedNajahi@users.noreply.github.com>
2026-04-27 20:00:37 +0530

    Remove base URL from vite.config.ts
    Removed base URL configuration from Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio-najahi/", // ⭐ ADD THIS LINE (VERY IMPORTANT)
-
   server: {
     host: "::",
     port: 8080,
